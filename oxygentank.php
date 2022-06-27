9456ac48d65f1c8410860176b47eb73e61e03666
M_F_A_A_K <99393366+MFAzadkhan@users.noreply.github.com>
2022-06-27 09:27:42 +0200

    update

--- oxygentank.php
+++ oxygentank.php
@@ -1,1 +1,9 @@
 <?php
+$tanksize=readline("Enter the tank size: ");
+$manopressure=readline("Enter the mano pressure: ");
+$useroxygenconsumption=readline("Enter the user oxygen consumption: ");
+$usertankcapcity=round( $tanksize*$manopressure/$useroxygenconsumption);
+
+{
+    echo "The tank capacity is: ".$usertankcapcity."\n";
+}
